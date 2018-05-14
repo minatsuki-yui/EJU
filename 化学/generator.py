fb9c0f42e218885fd02cc067e106578cbca673e2
yue <clavichord.minatsuki@gmail.com>
2018-05-15 01:49:53 +0900

    fix typo

--- 化学/generator.py
+++ 化学/generator.py
@@ -7,7 +7,7 @@ from yaml.reader import ReaderError
 class Generator(object):
     def __init__(self):
         self.all_notes = {}
-        self.defalut_dbname = 'まとめ'
+        self.default_dbname = 'まとめ'
         self.current_filename = ''
 
     def generate(self, dbname=''):
@@ -24,7 +24,7 @@ class Generator(object):
             pass
             # print(self.all_notes)
         if not dbname:
-            self.write_out(self.defalut_dbname + '.json')
+            self.write_out(self.default_dbname + '.json')
 
         print('\nfinished')
 
@@ -40,7 +40,7 @@ class Generator(object):
         del content
 
     def write_out(self, dbname):
-        self.all_notes['title'] = self.defalut_dbname
+        self.all_notes['title'] = self.default_dbname
         self.all_notes['vol'] = '＄'
         with open(dbname, 'w') as db:
             json.dump(self.all_notes, db, ensure_ascii=False)
